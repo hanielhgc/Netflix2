234bc7ecd961fc1d762bf0e6e6055b2bc047ad53
Pedro <nedehf@edu.unifor.br>
2017-05-23 08:32:37 -0300

    Mudanças para o Haniel

--- src/forms/UsuarioForm.java
+++ src/forms/UsuarioForm.java
@@ -2,20 +2,32 @@ package forms;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
+import javax.faces.bean.ViewScoped;
 
 import beans.Usuario;
 import persistence.UsuarioDao;
 
 @ManagedBean
 @RequestScoped
+@ViewScoped
 public class UsuarioForm {
 
-	
-
 	private Usuario usuario = new Usuario();
-	
+
+	@ManagedProperty(value = "#{usuarioLogado}")
+	private Usuario usuarioSessao;
+
+	public Usuario getUsuarioSessao() {
+		return usuarioSessao;
+	}
+
+	public void setUsuarioSessao(Usuario usuarioSessao) {
+		this.usuarioSessao = usuarioSessao;
+	}
+
 	public Usuario getUsuario() {
 		return usuario;
 	}
@@ -23,109 +35,75 @@ public class UsuarioForm {
 	public void setUsuario(Usuario usuario) {
 		this.usuario = usuario;
 	}
-
 	
-	
-	
-	
-	public String alterar(){
+	public String alterar() {
 		Usuario usuario1 = new Usuario();
 		UsuarioDao udao = new UsuarioDao();
 		FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("usuarioLogado", usuario1);
-	
+
 		udao.alterar(usuario1.getEmail(), usuario1);
-		return "InicioUsuario";	
-		}
-	
-	
-	
-	
-	
-	
-	
-	public String logout(){
-		
-		
+		return "InicioUsuario";
+	}
+
+	public String logout() {
+
 		FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
 		return "Login";
-		
+
 	}
-	
-	
-	
-	public String nome(){
-		
-		return usuario.getNome();
-	}
-	
-	
-	public String adicionar(){
+
+	public String adicionar() {
 		usuario.setPerfil("Cliente");
-	//Usuario usuario1 = new Usuario(usuario.getEmail(), usuario.getSenha(), usuario.getNome(), usuario.getPlano(), usuario.getCartao(), usuario.getPerfil());
+		// Usuario usuario1 = new Usuario(usuario.getEmail(),
+		// usuario.getSenha(), usuario.getNome(), usuario.getPlano(),
+		// usuario.getCartao(), usuario.getPerfil());
 		UsuarioDao udao = new UsuarioDao();
-		
-		if(usuario.getEmail().isEmpty() || usuario.getSenha().isEmpty() || usuario.getNome().isEmpty() || usuario.getPlano().isEmpty() || usuario.getCartao().isEmpty()){
-		
-			
+
+		if (usuario.getEmail().isEmpty() || usuario.getSenha().isEmpty() || usuario.getNome().isEmpty()
+				|| usuario.getPlano().isEmpty() || usuario.getCartao().isEmpty()) {
+
 			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Verifique os campos."));
 			return "Cadastro";
-		}else{
-		
+		} else {
 
 			udao.inserir(usuario);
 
-			
-		
-			
-		return "Login";	
+			return "Login";
 		}
-		
-		
-	
-		
-		
+
 	}
-	
-	
-	
-	
-	public String validar(){
-		
-		
+
+	public String validar() {
+
 		Usuario usuario1 = new Usuario();
 		UsuarioDao udao = new UsuarioDao();
 		usuario1 = udao.buscarPorEmail(usuario.getEmail());
-		
+
 		try {
 			System.out.println(usuario1.getPerfil());
 		} catch (NullPointerException e) {
-			
+
 			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Usuário e/ou senha incorretos."));
-			
+
 			return "Login";
-			
-			
+
 		}
-		
-		if(usuario.getEmail() != null && usuario != null && usuario.getSenha().equals(usuario1.getSenha())){
+
+		if (usuario.getEmail() != null && usuario != null && usuario.getSenha().equals(usuario1.getSenha())) {
 			FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("usuarioLogado", usuario1);
-			
-			
-			if(usuario1.getPerfil().equals("Administrador")){
+
+			if (usuario1.getPerfil().equals("Administrador")) {
 				return "InicioAdministrador";
-				}else{
-			
-					return "InicioUsuario";
-					}
-		
-			
-			
-		}else{
+			} else {
+
+				return "InicioUsuario";
+			}
+
+		} else {
 			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Usuário e/ou senha incorretos."));
-		return null;
+			return null;
 		}
-		
-		
+
 	}
-	
+
 }
